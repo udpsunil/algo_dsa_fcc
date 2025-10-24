24b91554397b20daf0ed3deb91a2459822b7eebc
Sunil Udupi <udpsunil@gmail.com>
2025-10-24 21:30:02 +0530

    Add BigOExamples class with O(1) and binary search methods

---
+++ big_o_example.py
@@ -0,0 +1,17 @@
+class BigOExamples:
+    # O(1)
+    def get_first_element(self, arr):
+        return arr[0]
+
+    def binary_search(self, arr: list[int], target: int) -> int:
+        left = 0
+        right = len(arr) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
